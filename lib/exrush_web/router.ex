814fc09092cbf6f2bfbb9a105a91d543a6ef5f69
Xavi Rodríguez <xavier.rodriguez@betika.com>
2021-07-19 20:04:35 +0200

    Fixed formatter issues

--- lib/exrush_web/router.ex
+++ lib/exrush_web/router.ex
@@ -2,22 +2,22 @@ defmodule ExrushWeb.Router do
   use ExrushWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {ExrushWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {ExrushWeb.LayoutView, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ExrushWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    live "/", PageLive, :index
+    live("/", PageLive, :index)
   end
 
   # Other scopes may use custom stacks.
@@ -36,8 +36,8 @@ defmodule ExrushWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: ExrushWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: ExrushWeb.Telemetry)
     end
   end
 end
